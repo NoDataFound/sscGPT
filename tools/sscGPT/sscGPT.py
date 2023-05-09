cd07af8cabe15bc6ffb6f969ef2415dc3ae44eed
Corian (Cory) Kennedy <cory@darkintel.io>
2023-05-09 17:47:56 -0500

    Update sscGPT.py

--- tools/sscGPT/sscGPT.py
+++ tools/sscGPT/sscGPT.py
@@ -273,10 +273,11 @@ with open(os.path.join(personas, f"{query_persona}.txt"), "r") as f:
                         stop=None,
                         temperature=1.0,
                     )
-                generated_text_chunks.append(completions.choices[0].text.strip())
-                query = completions.choices[0].text.strip()
-                assets = search_assets(query)
-                total_size =  num_chunks * chunk_size
+                    generated_text_chunks.append(completions.choices[0].text.strip())
+                    generated_text = '\n'.join(generated_text_chunks)
+                    query = completions.choices[0].text.strip()
+                    assets = search_assets(query)
+                    total_size =  num_chunks * chunk_size
                 col1, col2, col3 = st.columns(3)
                 col1.metric("HTML Word Count", total_size,total_size ) 
                 col2.metric("Token Packages", num_chunks,num_chunks ) 
@@ -284,7 +285,7 @@ with open(os.path.join(personas, f"{query_persona}.txt"), "r") as f:
                 st.markdown("----")
             except requests.exceptions.RequestException as e:
                 st.error(f"Error occurred while fetching the URL: {e}")
-            generated_text = '\n'.join(generated_text_chunks)
+            
             st.info("Generated Attack Surface Intelligence Query from URL")
             st.write(f"{generated_text}")
                     #st.write(total_size)  
